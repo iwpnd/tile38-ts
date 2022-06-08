da4d3ccc653b996f4985b1c745afde28e386ea10
Benjamin Ramser <legionaerr@googlemail.com>
2022-06-08 13:31:08 +0200

    ci: 👷 add release step with --dry-run

---
+++ release.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+    plugins: [
+        [
+            '@semantic-release/commit-analyzer',
+            {
+                preset: 'conventionalcommits',
+                releaseRules: [
+                    { type: 'docs', release: 'patch' },
+                    { type: 'perf', release: 'patch' },
+                ],
+            },
+        ],
+        [
+            '@semantic-release/release-notes-generator',
+            {
+                preset: 'conventionalcommits',
+                presetConfig: {
+                    types: [
+                        { type: 'feat', section: 'Features' },
+                        { type: 'fix', section: 'Bug Fixes' },
+                        { type: 'chore', section: 'Other' },
+                        { type: 'docs', section: 'Documentation' },
+                        { type: 'style', section: 'Other' },
+                        { type: 'refactor', section: 'Other' },
+                        { type: 'perf', section: 'Other' },
+                        { type: 'test', section: 'Other' },
+                        { type: 'build', section: 'Other' },
+                        { type: 'ci', section: 'Other' },
+                    ],
+                },
+            },
+        ],
+        '@semantic-release/changelog',
+        '@semantic-release/github',
+        '@semantic-release/npm',
+        '@semantic-release/git',
+    ],
+};
