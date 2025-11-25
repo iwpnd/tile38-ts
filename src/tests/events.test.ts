7668927d8ec5e9072699fb13ec5bda4dda726927
Benjamin Ramser <legionaerr@googlemail.com>
2025-11-25 10:56:30 +0100

    chore(deps): 🔗 bump ioredis to >5.8.0

--- src/tests/events.test.ts
+++ src/tests/events.test.ts
@@ -1,16 +1,12 @@
 import { Tile38 } from '..';
 
 describe('events', () => {
-    let tile38: Tile38;
-
-    afterEach(() => tile38.quit());
-
     it('should emit connect/ready/close/end', async () => {
         const connect = jest.fn();
         const ready = jest.fn();
         const close = jest.fn();
         const end = jest.fn();
-        tile38 = new Tile38()
+        const tile38 = new Tile38()
             .on('connect', connect)
             .on('ready', ready)
             .on('close', close)
@@ -25,20 +21,22 @@ describe('events', () => {
         expect(ready).toHaveBeenCalled();
         expect(close).toHaveBeenCalled();
         expect(end).toHaveBeenCalled();
+
+        await tile38.quit();
     });
 
     it('should emit error', async () => {
         const error = jest.fn();
-        tile38 = new Tile38(1234, { maxRetriesPerRequest: 0 }).on(
-            'error',
-            error
-        );
+        const tile38 = new Tile38(1234, {
+            maxRetriesPerRequest: 1,
+            retryStrategy: () => {
+                return;
+            },
+        }).on('error', error);
         expect(error).not.toHaveBeenCalled();
         await expect(tile38.ping()).rejects.toBeInstanceOf(Error);
-        expect(error).toHaveBeenCalledWith(
-            expect.objectContaining({
-                message: 'connect ECONNREFUSED 127.0.0.1:1234',
-            })
-        );
+        expect(error).toHaveBeenCalled();
+
+        await tile38.quit();
     });
 });
